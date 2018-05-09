a91fd1b037284ee2cb9be5e3dc81bb15b3e3f23d
Mike Wu <mikewu940327@gmail.com>
2018-05-09 18:09:44 -0500

    first

---
+++ JS-Interactive-table /index.js
@@ -0,0 +1,63 @@
+// Get references to the tbody element, input field and button
+var tbody = document.querySelector("tbody");
+var dateInput = document.querySelector("#date");
+var cityInput = document.querySelector("#city");
+var stateInput = document.querySelector("#state");
+var countryInput = document.querySelector("#country");
+var shapeInput = document.querySelector("#shape");
+var searchBtn = document.querySelector("#search");
+
+
+// Add an event listener to the searchButton, call handleSearchButtonClick when clicked
+searchBtn.addEventListener("click", handleSearchButtonClick);
+
+var filteredData = dataSet;
+function renderTable() {
+  
+  tbody.innerHTML = "";
+  for (var i = 0; i < filteredData.length; i++) {
+    var data = filteredData[i];
+    var fields = Object.keys(data);
+    var row = tbody.insertRow(i);
+    for (var j = 0; j < fields.length; j++) {
+      var field = fields[j];
+      var cell = row.insertCell(j);
+      cell.innerText = data[field];
+    };
+  };
+};
+
+
+renderTable();
+
+function handleSearchButtonClick() {
+
+  var addressDate = dateInput.value.trim();
+  var addressCity = cityInput.value.trim().toLowerCase();
+  var addressState = stateInput.value.trim().toLowerCase();
+  var addressCountry = countryInput.value.trim().toLowerCase();
+  var addressShape =  shapeInput.value.trim().toLowerCase();
+
+
+  function filterDataset (filteredData) {
+    var filtered = [];
+    for (var k = 0; k < filteredData.length; ++k) {
+    var item = filteredData[k];
+    if (item.datetime == addressDate && item.city.toLowerCase() == addressCity && item.shape.toLowerCase() == addressShape && item.state.toLowerCase() == addressState  
+      && item.country.toLowerCase() == addressCountry ) filtered.push(item);
+  };
+  
+   console.log(filtered);
+   return filtered;
+  
+  }
+
+  filteredData = filterDataset (filteredData);
+
+  renderTable();
+};
+
+
+
+
+
